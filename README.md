602f2ecd79b4449b0ec894c9ca1aae59938933d7
braynwash <f.tzgeraldz@gmail.com>
2023-10-09 10:03:46 -0400

    Started mobile nav

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Project by Elias Arriaga and Jeremy Black
 
-
+## Web 4 link
---
+++ app/app.js
@@ -0,0 +1,40 @@
+import { changePage } from "../model/model.js";
+
+function route() {
+    let hashTag = window.location.hash;
+    let pageID = hashTag.replace("#", "");
+    changePage(hashTag, pageID);
+}
+ 
+function initListeners() {
+    console.log("Test");
+    let navShow = false;
+    $(window).on("resize", (e) => {
+        if ($(window).width() > 768) {
+        $(".links").removeClass("hide");
+        } else {
+        $(".links").addClass("hide");    
+        }
+    });
+$(".hamburger").on("click", (e) => {
+    if (navShow) {
+        $(".links").removeClass("m-links");
+        $(".links").css("display", "none");
+        navShow = false;
+    } else {
+        $(".links").addClass("m-links");
+        $(".links").css("display", "flex");
+        navShow = true;
+    }
+    })
+}
+
+function initUrlListeners() {
+    $(window).on("hashchange", route);
+    route();
+}
+
+$(document).ready(function () {
+    initListeners();
+    initUrlListeners();
+})
